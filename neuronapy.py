f415f1576ea9ae8a8428a6a73d21577f3719b9b6
Gabriel Omar <omar.hinojoza13@gmail.com>
2019-08-27 19:52:02 -0500

    clase 5 y tarea termianda

--- neuronapy.py
+++ neuronapy.py
@@ -1,46 +1,70 @@
-from functools import reduce
+import matplotlib.pyplot as plt
+import random
 import numpy as np
 
 
-def mult_vect_scalar(data, scalar):
-    return data[0]*scalar + data[1] * scalar
+def act_fun(val):
+    return 1 if val > 0 else 0
 
 
-def adjust_w(w, est, res, data):
-    err = (est+0) - (res + 0)
-    merged = [data, err]
-    return w + reduce(mult_vect_scalar, merged)
-
-
-def adjust_b(b, est, res, etha): return b + etha * (est - res)
-
-
-def act_fun(w, data, b):
-    return np.dot(w, data) + b > 0
-
-
-w = np.random.rand(4, 2)
-b = np.random.rand()
-etha = 0.5
-results = [0, 0, 0, 1]
-
-training_set = [
-    [0, 1],
-    [1, 0],
+entradas = [
     [0, 0],
+    [1, 0],
+    [0, 1],
     [1, 1]
-
-
 ]
-not_converge = True
+# D = [0, 0, 0, 1]
+D = [0, 1, 1, 1]
+
+umbral = 0
+etha = 0.1
+peso1 = random.uniform(-1, 1)
+peso2 = random.uniform(-1, 1)
+print(peso1, peso2)
+errors = [True, True, True, True]
+salidas = [0, 1, 1, 0]
+converge = False
 idx = 0
-while not_converge:
-    y = act_fun(w[idx], training_set[idx], b)
-    if y != results[idx]:
-        w[idx] = adjust_w(w[idx], results[idx], y, training_set[idx])
-        b = adjust_b(b, results[idx], y, etha)
-    if idx+1 == len(training_set):
-        print(w)
-        print("\r")
-        idx = 0
+pasos = 0
+while True in errors:  # until converge
+    pasos += 1
+    #prod_punto = np.dot([peso1, peso2], entradas[idx])
+    prod_punto = (entradas[idx][0]*peso1 + entradas[idx][1]*peso2) + umbral
+    print(prod_punto)
+    print("El resultado del producto punto es: ", prod_punto)
+    y = act_fun(prod_punto)
+    salidas[idx] = y
+
+    print("Deseado: ", D[idx])
+    print("Obtenido: ", y)
+    error = D[idx] - y
+    if error != 0:
+        print("Recalculando pesos...")
+        peso1 = peso1 + etha * error * entradas[idx][0]
+        peso2 = peso2 + etha * error * entradas[idx][1]
+        print("pesos: ", [peso1, peso2])
+        umbral = umbral + etha*error
+        print("umbral: ", umbral)
+        errors[idx] = True
+    else:
+        errors[idx] = False
     idx += 1
+    if idx >= len(entradas):
+        x1 = np.linspace(-3, 3, 20)
+        x2 = (-peso1/peso2) * x1 - (umbral / peso2)
+        #graph = [[x, x*m+b]for x in np.linspace(-3, 3, 20)]
+        print("salida: ", y)
+        #print("linea: ", graph)
+        for entrada, step in zip(entradas, D):
+            plt.scatter(entrada[0], entrada[1],
+                        color="blue" if step else "red")
+
+        #plt.plot(x1, x2)
+        idx = 0
+
+print("Entrenamiento terminado, se obtuvo: ", salidas)
+print("Cantidad de iteraciones: ", pasos)
+x1 = np.linspace(-3, 3, 20)
+x2 = (-peso1/peso2) * x1 - (umbral / peso2)
+plt.plot(x1, x2)
+plt.show()
